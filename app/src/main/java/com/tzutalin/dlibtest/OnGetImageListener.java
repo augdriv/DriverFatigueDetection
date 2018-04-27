9c0c490051af11f9554bfd2b36a677771559419c
uholee92 <uholee92@gmail.com>
2018-04-27 18:36:23 +0900

    졸음 인식 코드 추가

---
+++ app/src/main/java/com/tzutalin/dlibtest/OnGetImageListener.java
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2016-present Tzutalin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tzutalin.dlibtest;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Handler;
+import android.os.Trace;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+
+import com.tzutalin.dlib.Constants;
+import com.tzutalin.dlib.FaceDet;
+import com.tzutalin.dlib.VisionDetRet;
+
+import junit.framework.Assert;
+
+import org.opencv.engine.OpenCVEngineInterface;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Class that takes in preview frames and converts the image to Bitmaps to process with dlib lib.
+ */
+public class OnGetImageListener implements OnImageAvailableListener {
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+
+    private static final int INPUT_SIZE = 224;
+    private static final String TAG = "OnGetImageListener";
+
+    private int mScreenRotation = 90;
+
+    private int mPreviewWdith = 0;
+    private int mPreviewHeight = 0;
+    private byte[][] mYUVBytes;
+    private int[] mRGBBytes = null;
+    private Bitmap mRGBframeBitmap = null;
+    private Bitmap mCroppedBitmap = null;
+
+    private boolean mIsComputing = false;
+    private Handler mInferenceHandler;
+
+    private Context mContext;
+    private FaceDet mFaceDet;
+    private TrasparentTitleView mTransparentTitleView;
+    private FloatingCameraWindow mWindow;
+    private Paint mFaceLandmardkPaint;
+
+    public void initialize(
+            final Context context,
+            final AssetManager assetManager,
+            final TrasparentTitleView scoreView,
+            final Handler handler) {
+        this.mContext = context;
+        this.mTransparentTitleView = scoreView;
+        this.mInferenceHandler = handler;
+        mFaceDet = new FaceDet(Constants.getFaceShapeModelPath());
+        mWindow = new FloatingCameraWindow(mContext);
+
+        mFaceLandmardkPaint = new Paint();
+        mFaceLandmardkPaint.setColor(Color.GREEN);
+        mFaceLandmardkPaint.setStrokeWidth(2);
+        mFaceLandmardkPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    public void deInitialize() {
+        synchronized (OnGetImageListener.this) {
+            if (mFaceDet != null) {
+                mFaceDet.release();
+            }
+
+            if (mWindow != null) {
+                mWindow.release();
+            }
+        }
+    }
+
+    private void drawResizedBitmap(final Bitmap src, final Bitmap dst) {
+
+        Display getOrient = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int orientation = Configuration.ORIENTATION_UNDEFINED;
+        Point point = new Point();
+        getOrient.getSize(point);
+        int screen_width = point.x;
+        int screen_height = point.y;
+        Log.d(TAG, String.format("screen size (%d,%d)", screen_width, screen_height));
+        if (screen_width < screen_height) {
+            orientation = Configuration.ORIENTATION_PORTRAIT;
+            mScreenRotation = 90;
+        } else {
+            orientation = Configuration.ORIENTATION_LANDSCAPE;
+            mScreenRotation = 0;
+        }
+
+        Assert.assertEquals(dst.getWidth(), dst.getHeight());
+        final float minDim = Math.min(src.getWidth(), src.getHeight());
+
+        final Matrix matrix = new Matrix();
+
+        // We only want the center square out of the original rectangle.
+        final float translateX = -Math.max(0, (src.getWidth() - minDim) / 2);
+        final float translateY = -Math.max(0, (src.getHeight() - minDim) / 2);
+        matrix.preTranslate(translateX, translateY);
+
+        final float scaleFactor = dst.getHeight() / minDim;
+        matrix.postScale(scaleFactor, scaleFactor);
+
+        // Rotate around the center if necessary.
+        if (mScreenRotation != 0) {
+            matrix.postTranslate(-dst.getWidth() / 2.0f, -dst.getHeight() / 2.0f);
+            matrix.postRotate(mScreenRotation);
+            matrix.postTranslate(dst.getWidth() / 2.0f, dst.getHeight() / 2.0f);
+        }
+
+        final Canvas canvas = new Canvas(dst);
+        canvas.drawBitmap(src, matrix, null);
+    }
+
+    @Override
+    public void onImageAvailable(final ImageReader reader) {
+        Image image = null;
+        try {
+            image = reader.acquireLatestImage();
+
+            if (image == null) {
+                return;
+            }
+
+            // No mutex needed as this method is not reentrant.
+            if (mIsComputing) {
+                image.close();
+                return;
+            }
+            mIsComputing = true;
+
+            Trace.beginSection("imageAvailable");
+
+            final Plane[] planes = image.getPlanes();
+
+            // Initialize the storage bitmaps once when the resolution is known.
+            if (mPreviewWdith != image.getWidth() || mPreviewHeight != image.getHeight()) {
+                mPreviewWdith = image.getWidth();
+                mPreviewHeight = image.getHeight();
+
+                Log.d(TAG, String.format("Initializing at size %dx%d", mPreviewWdith, mPreviewHeight));
+                mRGBBytes = new int[mPreviewWdith * mPreviewHeight];
+                mRGBframeBitmap = Bitmap.createBitmap(mPreviewWdith, mPreviewHeight, Config.ARGB_8888);
+                mCroppedBitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Config.ARGB_8888);
+
+                mYUVBytes = new byte[planes.length][];
+                for (int i = 0; i < planes.length; ++i) {
+                    mYUVBytes[i] = new byte[planes[i].getBuffer().capacity()];
+                }
+            }
+
+            for (int i = 0; i < planes.length; ++i) {
+                planes[i].getBuffer().get(mYUVBytes[i]);
+            }
+
+            final int yRowStride = planes[0].getRowStride();
+            final int uvRowStride = planes[1].getRowStride();
+            final int uvPixelStride = planes[1].getPixelStride();
+            ImageUtils.convertYUV420ToARGB8888(
+                    mYUVBytes[0],
+                    mYUVBytes[1],
+                    mYUVBytes[2],
+                    mRGBBytes,
+                    mPreviewWdith,
+                    mPreviewHeight,
+                    yRowStride,
+                    uvRowStride,
+                    uvPixelStride,
+                    false);
+
+            image.close();
+        } catch (final Exception e) {
+            if (image != null) {
+                image.close();
+            }
+            Log.e(TAG, "Exception!", e);
+            Trace.endSection();
+            return;
+        }
+
+        mRGBframeBitmap.setPixels(mRGBBytes, 0, mPreviewWdith, 0, 0, mPreviewWdith, mPreviewHeight);
+        drawResizedBitmap(mRGBframeBitmap, mCroppedBitmap);
+
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(mCroppedBitmap);
+        }
+
+        mInferenceHandler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!new File(Constants.getFaceShapeModelPath()).exists()) {
+                            mTransparentTitleView.setText("Copying landmark model to " + Constants.getFaceShapeModelPath());
+                            FileUtils.copyFileFromRawToOthers(mContext, R.raw.shape_predictor_68_face_landmarks, Constants.getFaceShapeModelPath());
+                        }
+
+                        long startTime = System.currentTimeMillis();
+                        List<VisionDetRet> results;
+                        synchronized (OnGetImageListener.this) {
+                            results = mFaceDet.detect(mCroppedBitmap);  // result는 얼굴 검출 데이터
+                        }
+                        long endTime = System.currentTimeMillis();
+                        mTransparentTitleView.setText("Time cost: " + String.valueOf((endTime - startTime) / 1000f) + " sec");
+                        // Draw on bitmap
+                        if (results != null) {
+                            for (final VisionDetRet ret : results) {
+                                float resizeRatio = 1.0f;
+                                Rect bounds = new Rect();
+                                bounds.left = (int) (ret.getLeft() * resizeRatio);
+                                bounds.top = (int) (ret.getTop() * resizeRatio);
+                                bounds.right = (int) (ret.getRight() * resizeRatio);
+                                bounds.bottom = (int) (ret.getBottom() * resizeRatio);
+                                Canvas canvas = new Canvas(mCroppedBitmap);
+                                canvas.drawRect(bounds, mFaceLandmardkPaint);
+
+                                // Draw landmark
+                                ArrayList<Point> landmarks = ret.getFaceLandmarks();
+                                if (detectSleep(landmarks)) {
+                                    mFaceLandmardkPaint.setColor(Color.RED);
+                                    Log.d("SLEEPDETECTION", "true");
+                                }
+                                else {
+                                    mFaceLandmardkPaint.setColor(Color.GREEN);
+                                    Log.d("SLEEPDETECTION", "false");
+                                }
+
+                                for (Point point : landmarks) {
+                                    int pointX = (int) (point.x * resizeRatio);
+                                    int pointY = (int) (point.y * resizeRatio);
+                                    canvas.drawCircle(pointX, pointY, 2, mFaceLandmardkPaint);
+                                }
+                            }
+                        }
+
+                        mWindow.setRGBBitmap(mCroppedBitmap);
+                        mIsComputing = false;
+                    }
+                });
+
+        Trace.endSection();
+    }
+
+//    double compute_EAR(std::vector<cv::Point> vec)
+//    {
+//
+//        double a = cv::norm(cv::Mat(vec[1]), cv::Mat(vec[5]));
+//        double b = cv::norm(cv::Mat(vec[2]), cv::Mat(vec[4]));
+//        double c = cv::norm(cv::Mat(vec[0]), cv::Mat(vec[3]));
+//        //compute EAR
+//        double ear = (a + b) / (2.0 * c);
+//        return ear;
+//    }
+
+    boolean detectSleep(ArrayList<Point> face) {
+
+        ArrayList<Point> leftEye = new ArrayList<Point>();  // 왼쪽 눈 포인트들의 좌표
+        ArrayList<Point> rightEye = new ArrayList<Point>(); // 오른쪽 눈 포인트들의 좌표
+
+        double leftEar, rightEar;   //
+
+    // leftEye 초기화
+        for (int i = 36; i < 42; ++i) {
+            Point p = face.get(i);
+            leftEye.add(p);
+        }
+    // rightEye 초기화
+        for (int i = 42; i < 48; ++i) {
+            Point p = face.get(i);
+            rightEye.add(p);
+        }
+
+        rightEar = computeEar(rightEye);
+        leftEar = computeEar(leftEye);
+
+        if ((rightEar + leftEar) / 2 < 0.2) {
+            // 잔다
+            return true;
+        }
+        else {
+            // 안 잔다
+            return false;
+        }
+    }
+
+    double computeEar(ArrayList<Point> eye) {
+        double a = (eye.get(1).x - eye.get(5).x)*(eye.get(1).x - eye.get(5).x) + (eye.get(1).y - eye.get(5).y)*(eye.get(1).y - eye.get(5).y);
+        double b = (eye.get(2).x - eye.get(4).x)*(eye.get(2).x - eye.get(4).x) + (eye.get(2).y - eye.get(4).y)*(eye.get(2).y - eye.get(4).y);
+        double c = (eye.get(0).x - eye.get(3).x)*(eye.get(0).x - eye.get(3).x) + (eye.get(0).y - eye.get(3).y)*(eye.get(0).y - eye.get(3).y);
+
+        double ear = (Math.sqrt(a) + Math.sqrt(b)) / (2.0 + Math.sqrt(c));
+        return ear;
+    }
+}
