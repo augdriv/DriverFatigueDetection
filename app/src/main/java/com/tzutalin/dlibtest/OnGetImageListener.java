040dc7c12bd429e8b0e754adc730600faab82022
uholee92 <uholee92@gmail.com>
2018-04-28 16:17:44 +0900

    졸음 감지 코드 따로 분리.

--- app/src/main/java/com/tzutalin/dlibtest/OnGetImageListener.java
+++ app/src/main/java/com/tzutalin/dlibtest/OnGetImageListener.java
@@ -42,6 +42,7 @@ import android.view.WindowManager;
 import com.tzutalin.dlib.Constants;
 import com.tzutalin.dlib.FaceDet;
 import com.tzutalin.dlib.VisionDetRet;
+import com.uholee.sleepdetection.SleepDetection;
 
 import junit.framework.Assert;
 
@@ -260,7 +261,7 @@ public class OnGetImageListener implements OnImageAvailableListener {
 
                                 // Draw landmark
                                 ArrayList<Point> landmarks = ret.getFaceLandmarks();
-                                if (detectSleep(landmarks)) {
+                                if (SleepDetection.checkSleep(landmarks)) {
                                     mFaceLandmardkPaint.setColor(Color.RED);
                                     Log.d("SLEEPDETECTION", "true");
                                 }
@@ -295,44 +296,4 @@ public class OnGetImageListener implements OnImageAvailableListener {
 //        double ear = (a + b) / (2.0 * c);
 //        return ear;
 //    }
-
-    boolean detectSleep(ArrayList<Point> face) {
-
-        ArrayList<Point> leftEye = new ArrayList<Point>();  // 왼쪽 눈 포인트들의 좌표
-        ArrayList<Point> rightEye = new ArrayList<Point>(); // 오른쪽 눈 포인트들의 좌표
-
-        double leftEar, rightEar;   //
-
-    // leftEye 초기화
-        for (int i = 36; i < 42; ++i) {
-            Point p = face.get(i);
-            leftEye.add(p);
-        }
-    // rightEye 초기화
-        for (int i = 42; i < 48; ++i) {
-            Point p = face.get(i);
-            rightEye.add(p);
-        }
-
-        rightEar = computeEar(rightEye);
-        leftEar = computeEar(leftEye);
-
-        if ((rightEar + leftEar) / 2 < 0.2) {
-            // 잔다
-            return true;
-        }
-        else {
-            // 안 잔다
-            return false;
-        }
-    }
-
-    double computeEar(ArrayList<Point> eye) {
-        double a = (eye.get(1).x - eye.get(5).x)*(eye.get(1).x - eye.get(5).x) + (eye.get(1).y - eye.get(5).y)*(eye.get(1).y - eye.get(5).y);
-        double b = (eye.get(2).x - eye.get(4).x)*(eye.get(2).x - eye.get(4).x) + (eye.get(2).y - eye.get(4).y)*(eye.get(2).y - eye.get(4).y);
-        double c = (eye.get(0).x - eye.get(3).x)*(eye.get(0).x - eye.get(3).x) + (eye.get(0).y - eye.get(3).y)*(eye.get(0).y - eye.get(3).y);
-
-        double ear = (Math.sqrt(a) + Math.sqrt(b)) / (2.0 + Math.sqrt(c));
-        return ear;
-    }
 }
---
+++ app/src/main/java/com/uholee/sleepdetection/SleepDetection.java
@@ -0,0 +1,55 @@
+package com.uholee.sleepdetection;
+
+import android.graphics.Point;
+
+import java.util.ArrayList;
+
+/**
+ * Created by uhole on 2018-04-28.
+ */
+
+public class SleepDetection {
+    private static SleepDetection mObject;
+
+    public static boolean checkSleep(ArrayList<Point> face) {
+        return mObject.checkSleep_method(face);
+    }
+    private boolean checkSleep_method(ArrayList<Point> face) {
+        ArrayList<Point> leftEye = new ArrayList<Point>();  // 왼쪽 눈 포인트들의 좌표
+        ArrayList<Point> rightEye = new ArrayList<Point>(); // 오른쪽 눈 포인트들의 좌표
+
+        double leftEar, rightEar;   //
+
+        // leftEye 초기화
+        for (int i = 36; i < 42; ++i) {
+            Point p = face.get(i);
+            leftEye.add(p);
+        }
+        // rightEye 초기화
+        for (int i = 42; i < 48; ++i) {
+            Point p = face.get(i);
+            rightEye.add(p);
+        }
+
+        rightEar = computeEar(rightEye);
+        leftEar = computeEar(leftEye);
+
+        if ((rightEar + leftEar) / 2 < 0.2) {
+            // 잔다
+            return true;
+        }
+        else {
+            // 안 잔다
+            return false;
+        }
+    }
+
+    double computeEar(ArrayList<Point> eye) {
+        double a = (eye.get(1).x - eye.get(5).x)*(eye.get(1).x - eye.get(5).x) + (eye.get(1).y - eye.get(5).y)*(eye.get(1).y - eye.get(5).y);
+        double b = (eye.get(2).x - eye.get(4).x)*(eye.get(2).x - eye.get(4).x) + (eye.get(2).y - eye.get(4).y)*(eye.get(2).y - eye.get(4).y);
+        double c = (eye.get(0).x - eye.get(3).x)*(eye.get(0).x - eye.get(3).x) + (eye.get(0).y - eye.get(3).y)*(eye.get(0).y - eye.get(3).y);
+
+        double ear = (Math.sqrt(a) + Math.sqrt(b)) / (2.0 + Math.sqrt(c));
+        return ear;
+    }
+}
